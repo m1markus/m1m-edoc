f5ea7109831ed314dfcbdaf9a757a122971bea0d
Markus Müller <mmm7@bluewin.ch>
2026-01-16 00:28:50 +0100

    Send html email composed with mvel template engine

--- src/main/java/ch/m1m/edoc/GreetingResource.java
+++ src/main/java/ch/m1m/edoc/GreetingResource.java
@@ -9,6 +9,8 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -33,7 +35,10 @@ public class GreetingResource {
             log.info("uuid equals failed");
         }
 
-        //emailService.send("mmm7@bluewin.ch", "test-2", "login-link-confirm", null);
+        Map<String, Object> emailContext = new HashMap<>();
+        emailContext.put("firstname", "Markus");
+
+        emailService.send("mmm7@bluewin.ch", "test-2", "login-link-confirm", emailContext);
 
         return "Hello from Quarkus REST";
     }
--- src/main/java/ch/m1m/edoc/email/EmailService.java
+++ src/main/java/ch/m1m/edoc/email/EmailService.java
@@ -6,6 +6,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.mvel2.templates.TemplateRuntime;
 
 import java.util.Map;
 
@@ -16,21 +17,58 @@ public class EmailService {
     @Inject
     Mailer mailer;
 
-    @ConfigProperty(name = "quarkus.mailer.password")
+    @ConfigProperty(name = "quarkus.mailer.password", defaultValue = "email-password-is-not-set")
     String smtpUserPassword;
 
     public void send(String to,
                      String subject,
                      String templateName,
-                     Map map) {
+                     Map<String, Object> map) {
         log.info("EmailService send called with...");
 
-        mailer.send(Mail.withText(to, subject, getBody(templateName)));
+        // send text mail only
+        //mailer.send(Mail.withText(to, subject, getTextBody(templateName)));
 
-        log.info("using password {}", smtpUserPassword);
+        // use mvel2 template engine
+        String bodyTemplateRaw = getMvelTemplate("toto");
+        String htmlMailBody = (String) TemplateRuntime.eval(bodyTemplateRaw, map);
+
+        // send html mail
+        mailer.send(Mail.withHtml(to, subject, htmlMailBody));
+
+        //log.debug("using password {}", smtpUserPassword);
     }
 
-    private String getBody(String templateName) {
+    // http://mvel.documentnode.com/#language-guide-for-2.0
+    //
+    private String getMvelTemplate(String templateName) {
+
+        return """
+                <h2>dear @{firstname}</h2>
+                <p>
+                this is the body text test message
+                with no more expanded parameters.</p>
+                <p>
+                this is the body text test message
+                with no more expanded parameters.</p>
+                <a href="https://www.20min.ch">confirm your e-mail address</a>
+                """;
+    }
+
+    private String getHtmlBody(String templateName) {
+        return """
+                <h2>dear %firstname</h2>
+                <p>
+                this is the body text test message
+                with no more expanded parameters.</p>
+                <p>
+                this is the body text test message
+                with no more expanded parameters.</p>
+                <a href="https://www.20min.ch">confirm your e-mail address</a>
+                """;
+    }
+
+    private String getTextBody(String templateName) {
         return """
                 dear %sirname,
                 this is the body text test message
